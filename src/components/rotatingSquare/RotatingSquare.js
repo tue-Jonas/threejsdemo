27f20569fdef3e8940c8c3c3c34070180e650ab6
Jonas Tuechler <jonas.tuechler@gmail.com>
2023-10-02 10:40:51 +0200

    rotating cube

---
+++ src/components/rotatingSquare/RotatingSquare.js
@@ -0,0 +1,52 @@
+import React, {useEffect, useRef} from 'react';
+import * as THREE from 'three';
+
+const RotatingSquare = () => {
+    const mountRef = useRef(null);
+
+    useEffect(() => {
+        const width = mountRef.current.clientWidth;
+        const height = mountRef.current.clientHeight;
+
+        // Setup scene, camera, and renderer
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer({antialias: true});
+        renderer.setSize(width, height);
+        mountRef.current.appendChild(renderer.domElement);
+
+        // Create a geometry and a material then combine them into a mesh
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({color: 0x0077ff});
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+        camera.position.z = 5;
+
+        // Render loop
+        const animate = () => {
+            requestAnimationFrame(animate);
+
+            // Rotate the cube
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+
+            renderer.render(scene, camera);
+        };
+
+        animate();
+
+        // Cleanup on unmount
+        return () => {
+            geometry.dispose();
+            material.dispose();
+            mountRef.current.removeChild(renderer.domElement);
+            renderer.dispose();  // This will also dispose of the scene and camera
+        };
+
+    }, []);
+
+    return <div ref={mountRef} style={{width: '400px', height: '400px'}}/>;
+};
+
+export default RotatingSquare;

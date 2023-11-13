7fc7a609e006f50d81a0c650858cb2575a0bfc0a
Jonas Tuechler <jonas.tuechler@gmail.com>
2023-11-13 11:57:54 +0100

    animation running smooth now

--- src/components/lighting/Lighting.js
+++ src/components/lighting/Lighting.js
@@ -1,92 +1,102 @@
-import React, {useEffect, useRef} from "react";
+import React, {useEffect, useRef} from 'react';
 import {
     AmbientLight,
     AxesHelper,
     BoxGeometry,
-    Color, DirectionalLight, DirectionalLightHelper,
+    DirectionalLight,
+    DirectionalLightHelper,
     GridHelper,
     Mesh,
-    MeshBasicMaterial, MeshPhongMaterial,
     MeshStandardMaterial,
     PerspectiveCamera,
     PlaneGeometry,
-    Scene, SphereGeometry,
+    Scene,
+    SphereGeometry,
     WebGLRenderer
 } from 'three';
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 const Lighting = () => {
     const canvasRef = useRef(null);
-    let controls;
 
     useEffect(() => {
         const scene = new Scene();
         const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         camera.position.set(3, 3, 3);
 
-        // Assuming the OrbitControls are correctly imported, they should be attached to the camera and canvas here
-        controls = new OrbitControls(camera, canvasRef.current);
-
         const renderer = new WebGLRenderer({canvas: canvasRef.current});
         renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.shadowMap.enabled = true;
 
+        const controls = new OrbitControls(camera, canvasRef.current);
+
+        // Helpers
         const axesHelper = new AxesHelper(5);
         scene.add(axesHelper);
-
         const gridHelper = new GridHelper(20);
         scene.add(gridHelper);
 
+        // Box
         const boxGeometry = new BoxGeometry();
-        const boxMaterial = new MeshStandardMaterial({color: Color.NAMES.green});
+        const boxMaterial = new MeshStandardMaterial({color: 0x00ff00});
         const box = new Mesh(boxGeometry, boxMaterial);
         scene.add(box);
 
-        const planeGeometry = new PlaneGeometry(10, 10);
-        const planeMaterial = new MeshBasicMaterial({color: Color.NAMES.white});
+        // Plane
+        const planeGeometry = new PlaneGeometry(15, 15);
+        const planeMaterial = new MeshStandardMaterial({color: 0xffffff});
         const plane = new Mesh(planeGeometry, planeMaterial);
-        plane.rotation.x = -90 * Math.PI / 180
-        scene.add(plane)
+        plane.rotation.x = -Math.PI / 2;
+        plane.receiveShadow = true;
+        scene.add(plane);
 
+        // Sphere
         const sphereGeometry = new SphereGeometry();
-        const sphereMaterial = new MeshPhongMaterial({
-            color: Color.NAMES.red,
-            specular: Color.NAMES.white
-        });
+        const sphereMaterial = new MeshStandardMaterial({color: 0xff0000});
         const sphere = new Mesh(sphereGeometry, sphereMaterial);
-        sphere.position.x = 3;
-        sphere.position.y = 2;
+        sphere.position.set(3, 2, 0);
+        sphere.castShadow = true;
         scene.add(sphere);
 
-        const ambientLight = new AmbientLight(Color.NAMES.darkgray);
+        // Lights
+        const ambientLight = new AmbientLight(0x404040);
         scene.add(ambientLight);
-
-        const directionalLight = new DirectionalLight(Color.NAMES.white, 1.0);
-        directionalLight.position.set(-10, 17, 0)
+        const directionalLight = new DirectionalLight(0xffffff, 1);
+        directionalLight.position.set(-10, 17, 0);
+        directionalLight.castShadow = true;
         scene.add(directionalLight);
+        scene.add(new DirectionalLightHelper(directionalLight));
 
-        const directionalLightHelper = new DirectionalLightHelper(directionalLight);
-        scene.add(directionalLightHelper);
+        // Animation loop
+        const animate = (delay) => {
+            requestAnimationFrame((delay) => animate(delay));
 
-        const animate = () => {
-            requestAnimationFrame(animate);
-            controls.update(); // Only required if controls.enableDamping or controls.autoRotate are set to true
+            sphere.position.y = Math.sin(delay / 1000) + 2;
+
+            controls.update();
             renderer.render(scene, camera);
         };
+        animate(0);
 
-        animate();
-
-        // Clean up on component unmount
+        // Clean up
         return () => {
             controls.dispose();
             renderer.dispose();
-        };
-    }, []); // Empty dependency array means this effect runs once on mount
+            scene.clear();
 
-    return (
-        <div style={{width: "100vw", height: "100vh", overflow: "hidden"}}>
-            <canvas ref={canvasRef}/>
-        </div>
-    );
+            // Dispose geometries and materials
+            boxGeometry.dispose();
+            boxMaterial.dispose();
+            planeGeometry.dispose();
+            planeMaterial.dispose();
+            sphereGeometry.dispose();
+            sphereMaterial.dispose();
+        };
+    }, []);
+
+    return <div style={{width: '100vw', height: '100vh', overflow: 'hidden'}}>
+        <canvas ref={canvasRef}/>
+    </div>;
 };
 
 export default Lighting;

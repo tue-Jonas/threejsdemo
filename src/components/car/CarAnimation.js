b43643369b9ea7ce7d6c710a8c7c76e654f33a85
Jonas Tüchler <jonas.tuechler@gmail.com>
2024-01-05 12:42:27 +0100

    improved lighting

--- src/components/car/CarAnimation.js
+++ src/components/car/CarAnimation.js
@@ -7,37 +7,44 @@ import Loader from "../Loader";
 const CarAnimation = () => {
     return (
         <Canvas className={"vh-100"} shadows>
-            <color args={[0, 0, 0]} attach="background"/>
+    <color args={[0, 0, 0]} attach="background"/>
 
-            <OrbitControls
-                target={[0, 0.35, 0]}
-            />
-            <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]}/>
+    <OrbitControls
+        target={[0, 0.35, 0]}
+    />
+    <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]}/>
 
-            <Suspense fallback={<Loader />}>
-                <Car/>
-            </Suspense>
+    <Suspense fallback={<Loader />}>
+        <Car/>
+    </Suspense>
 
-            <ambientLight intensity={1}/>
+    <ambientLight intensity={1}/>
 
-            <spotLight
-                color={[1, 1, 1]}
-                intensity={500}
-                angle={0.6}
-                penumbra={10.5}
-                position={[5, 5, 0]}
-            />
-            <spotLight
-                color={[1, 1, 1]}
-                intensity={500}
-                angle={0.6}
-                penumbra={0.5}
-                position={[-5, 5, 0]}
-            />
+    <spotLight
+        color={[1, 1, 1]}
+        intensity={400}
+        angle={0.6}
+        penumbra={1}
+        position={[5, 5, 0]}
+    />
+    <spotLight
+        color={[1, 1, 1]}
+        intensity={400}
+        angle={0.6}
+        penumbra={1}
+        position={[-5, 5, 0]}
+    />
+    <spotLight
+        color={[1, 1, 1]}
+        intensity={500}
+        angle={0.6}
+        penumbra={0.5}
+        position={[0, 5, -5]}
+    />
 
-            {/*<axesHelper args={[5]}/>*/}
-            {/*<gridHelper args={[20, 20]}/>*/}
-        </Canvas>
+    {/*<axesHelper args={[5]}/>*/}
+    {/*<gridHelper args={[20, 20]}/>*/}
+</Canvas>
     );
 };
 
